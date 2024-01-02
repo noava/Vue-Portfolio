f0b758e023fcf74ca0af72cdfd143ee8b0228576
Niklas Vezina <niklasv@uia.no>
2024-01-02 03:12:13 +0100

    First Commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <h1 style="margin-right: 2rem;">Website under development</h1>
+    <h1 class="orange">- Noava</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HomeView',
+    components: {},
+  }
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .home {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+
+}
+</style>
