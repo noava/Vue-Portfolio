141eab35477afdeacf7a36d9ebd14903348dd9d2
Noava <51932020+noava@users.noreply.github.com>
2024-02-29 00:52:15 +0100

    testing multiple descriptions

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,13 @@
 <template>
+  <head>
+    <meta property="og:description" content="A Portfolio of Niklas's Projects">
+    <title>Niklas's HomePage</title>
+
+  </head>
   <div class="home">
     <h1 style="margin-right: 2rem;">Website under development</h1>
     <h1 class="orange">- Noava</h1>
+    <input type="text">hey
   </div>
 </template>
 
